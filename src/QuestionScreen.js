1af8170c31eb7b8333cfd3e68aba3029386ad9e7
AyyanFawad <af06526@st.habib.edu.pk>
2023-12-14 20:03:33 +0500

    Updated index.css and QuestionScreen.js

--- src/QuestionScreen.js
+++ src/QuestionScreen.js
@@ -1,40 +1,98 @@
-
 import React, { useState } from 'react';
 import './index.css';
-import spinImage from './assets/spin.png'; // Ensure you have spin.png in your src folder
+import spinImage from './assets/spin.png'; // Ensure you have the correct path
+
+const questions = [
+  {
+    text: 'How long was your commute Today?',
+    options: [
+      'Less than 10 minutes',
+      '10-20 mins',
+      '20-30 mins',
+      '30-45 mins',
+      'More than 45 mins',
+    ],
+    image: require('./assets/cf.png'), // Replace with the correct path for your image
+    imageFact: "This picture is about a carbon footprint. They are very difficult to calculate and coding their applications frontend is even more annoying.",
+  },
+  {
+    text: 'What is your name?',
+    options: ['Ayyan', 'Shafay', 'Ayesha', 'Hafsa', 'random'],
+    image: require('./assets/e5.png'), // Replace with the correct path for your image
+    imageFact: "Wow thats a cool name",
+  },
+  // Add more questions as needed
+];
 
 function QuestionScreen() {
-  const [spinSpeed, setSpinSpeed] = useState('0s');
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-  const handleCommuteTime = (time) => {
-    // The spin duration is inversely proportional to the commute time
-    const speeds = {
-      'less': '10s',
-      '10-20': '8s',
-      '20-30': '6s',
-      '30-45': '4s',
-      'more': '2s'
-    };
-    setSpinSpeed(speeds[time]);
+  const handleAnswer = (answer) => {
+    // Handle the response for the current question here
+    console.log(`Answer for question ${currentQuestion + 1}: ${answer}`);
+
+    // Move to the next question
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+      setSelectedOption(null); // Reset selected option for the new question
+    }
   };
 
   return (
     <div className="QuestionScreen">
       <header className="Screen-header">
-        <div className="animation-box">
-        <img src={spinImage} alt="spinning" style={{ 
-            animation: `spin ${spinSpeed} linear infinite` 
-          }}/>
+        <div className="image-container">
+          <img
+            src={questions[currentQuestion].image}
+            alt={`question ${currentQuestion + 1}`}
+            className="question-image"
+          />
+          <div className="image-fact">
+            {questions[currentQuestion].imageFact}
+          </div>
         </div>
         <div className="commute-question">
-          <h1>How long was your <br></br> commute Today?</h1>
-          <button onClick={() => handleCommuteTime('less')}>Less than 10 minutes</button>
-          <button onClick={() => handleCommuteTime('10-20')}>10-20 mins</button>
-          <button onClick={() => handleCommuteTime('20-30')}>20-30 mins</button>
-          <button onClick={() => handleCommuteTime('30-45')}>30-45 mins</button>
-          <button onClick={() => handleCommuteTime('more')}>More than 45 mins</button>
+          <h1>{questions[currentQuestion].text}</h1>
+          {questions[currentQuestion].options.map((option, index) => (
+            <button
+              key={index}
+              onClick={() => setSelectedOption(option)}
+              className={selectedOption === option ? 'selected' : ''}
+            >
+              {option}
+            </button>
+          ))}
         </div>
       </header>
+      <div className="navigation-buttons">
+        <button
+          onClick={() =>
+            setCurrentQuestion((prevQuestion) =>
+              Math.max(prevQuestion - 1, 0)
+            )
+          }
+          disabled={currentQuestion === 0}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() =>
+            setCurrentQuestion((prevQuestion) =>
+              Math.min(prevQuestion + 1, questions.length - 1)
+            )
+          }
+          disabled={currentQuestion === questions.length - 1}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => handleAnswer(selectedOption)}
+          disabled={selectedOption === null}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
