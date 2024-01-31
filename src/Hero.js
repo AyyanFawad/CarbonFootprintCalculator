cb8b5150c9be6878ec7ef7748605eb2cb43cff45
AyyanFawad <af06526@st.habib.edu.pk>
2024-01-31 13:05:49 +0500

    Refactored code for hero.js and index.css

--- src/Hero.js
+++ src/Hero.js
@@ -10,13 +10,8 @@ const Hero = () => {
 
     return (
         <div className="hero-container">
-            <div className="hero-content">
+            <div className="hero-image-container">
                 <img src={cf} alt="cf" className="cf-image" />
-                <div className="text-container">
-                    {/* <a>Source: Carbon study by HBL and Students of Habib University. Additional resources here.</a><br></br>
-                <a>By using the Carbon Footprint Calculator, I agree to the Terms of Use and Privacy Policy.</a> */}
-
-                </div>
             </div>
             <div className="text-beside-image">
                 <div className="whats-ur-cfc">WHAT'S YOUR <br></br>CARBON <br></br>FOOTPRINT?</div>
