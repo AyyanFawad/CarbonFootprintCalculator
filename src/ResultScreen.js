e1dfa596b7ae1b849a97f1cec1057726f3a615ee
AyyanFawad <af06526@st.habib.edu.pk>
2024-02-05 19:07:53 +0500

    Added pie chart and refactored resultscreen

--- src/ResultScreen.js
+++ src/ResultScreen.js
@@ -47,40 +47,32 @@ const ResultScreen = () => {
     return (
         <div className='ResultScreen'>
             <div className='ResultBoxes'>
-                <section className="result-number-container">
-                    <div className='result-rectangle'>
-                        <div className="result-number">
-                            <span>{resultPercentage}</span>
-                            <div className='result-number-text'>Tonnes Carbon Footprint emitted</div>
-                        </div>
-                    </div>
-                </section>
-
-                <section className='result-fact-1-container'>
-                    <div className='fact-1-rectangle'>
-                        <div className='fact-1-text'>
-                            Your footprint is equal to x tonnes.
-                        </div>
-                    </div>
-                </section>
-
-                <section className='result-fact-2-container'>
-                    <div className='fact-2-rectangle'>
-                        <div className='fact-2-text'>
-                            The average Pakistani emits xyz tonnes daily.
-                        </div>
-                    </div>
-                </section>
-            </div>
-            <section className='breakdown-container'>
-                <div className='breakdown-rectangle'>
-                    <div className='breakdown-text'>The breakdown of your Carbon Footprint is as follows:</div>
-                    <div className='doughnut-chart'>
-                        <Doughnut data={data} options={options}>
-                        </Doughnut>
+                <div className='result-rectangle'>
+                    <div className="result-number">
+                        <span>{resultPercentage}</span>
+                        <div className='result-number-text'>Tonnes CO2 emitted</div>
                     </div>
                 </div>
-            </section>
+
+                <div className='fact-1-rectangle'>
+                    <div className='fact-1-text'>
+                        Your footprint is equal to x tonnes.
+                    </div>
+                </div>
+
+                <div className='fact-2-rectangle'>
+                    <div className='fact-2-text'>
+                        The average Pakistani emits xyz tonnes daily.
+                    </div>
+                </div>
+            </div>
+            <div className='breakdown-rectangle'>
+                <div className='breakdown-text'>The breakdown of your Carbon Footprint is as follows:</div>
+                <div className='doughnut-chart'>
+                    <Doughnut data={data} options={options}>
+                    </Doughnut>
+                </div>
+            </div>
         </div>
     );
 };
